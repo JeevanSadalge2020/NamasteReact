78a4b2d98fb8e35fb6f72e560cbb5d2fee9676ab
jeevan <jeevanhrx@gmail.com>
2024-02-07 18:48:23 +0530

    React component in all its variations

--- 3. Laying the foundation/index.js
+++ 3. Laying the foundation/index.js
@@ -3,9 +3,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const HeadingComponent = () => {
-  return <h1>Hello React, from functional component</h1>;
+const Title = () => <h1> Namaste React </h1>;
+
+const Aim = function () {
+  return <p>I am currently learning React</p>;
 };
+
+const name = <span>My name is Jeevan</span>;
+
+const bio = <p>{name}, and I am a JS developer.</p>;
+
+const myBirthYear = 1992;
+
+function HeadingComponent() {
+  return (
+    <div className="container">
+      <Title />
+      <h2> This is a great tutorial on React..! </h2>
+      <h3>
+        {bio}My age is {2024 - myBirthYear}
+      </h3>
+      {Aim()}
+    </div>
+  );
+}
 
 let root = ReactDOM.createRoot(document.querySelector(".root"));
 
