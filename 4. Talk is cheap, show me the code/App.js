30c68ac2080f5e4cf55ceec6589e02597b782be7
jeevan <jeevanhrx@gmail.com>
2024-02-13 00:25:16 +0530

    cards rendered

--- 4. Talk is cheap, show me the code/App.js
+++ 4. Talk is cheap, show me the code/App.js
@@ -4,26 +4,122 @@ import ReactDOM from "react-dom/client";
 const Header = () => {
   return (
     <header>
-      <div className="logo-div">
-        <img
-          className="logo"
-          src="https://images.unsplash.com/photo-1557053964-937650b63311?q=80&w=1597&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
+      <div className="container">
+        <nav className="navbar">
+          <div className="logo-div">
+            <img
+              className="logo"
+              src="https://images.unsplash.com/photo-1557053964-937650b63311?q=80&w=1597&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Cart</li>
+          </ul>
+        </nav>
       </div>
-      <nav className="nav">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Cart</li>
-        </ul>
-      </nav>
     </header>
   );
 };
 
+const RestCard = (props) => {
+  return (
+    <article className="restaurant-card">
+      <div className="restaurant-card-image-container">
+        <img
+          src={props.restImgUrl}
+          alt="none"
+          className="restaurant-card-image"
+        />
+      </div>
+      <div className="restaurant-card-details">
+        <h3 className="restaurant-card-title">{props.restName}</h3>
+        <p className="restaurant-card-menu">{props.restCuisine}</p>
+        <div className="restaurant-card-other-details">
+          <h4>{props.restRating}</h4>
+          <span>{props.restDeliveryTime}</span>
+          <span>Rs.{props.restCuisinePrice} (For Two)</span>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const Main = () => {
+  return (
+    <main className="restaurant-cards">
+      <div className="container">
+        <RestCard
+          restName="KFC"
+          restCuisine="Burgur, Pizza, Chicken"
+          restImgUrl="https://images.pexels.com/photos/16474897/pexels-photo-16474897/free-photo-of-a-plate-of-meat-barbecue-with-onions-and-pomegranate-seeds.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+        <RestCard
+          restName="Niyaaz Biryani"
+          restCuisine="Biryani"
+          restImgUrl="https://images.pexels.com/photos/12737817/pexels-photo-12737817.jpeg?auto=compress&cs=tinysrgb&w=600"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+        <RestCard
+          restName="Niyaaz Biryani"
+          restCuisine="Biryani"
+          restImgUrl="https://images.pexels.com/photos/12737817/pexels-photo-12737817.jpeg?auto=compress&cs=tinysrgb&w=600"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+        <RestCard
+          restName="Niyaaz Biryani"
+          restCuisine="Biryani"
+          restImgUrl="https://images.pexels.com/photos/12737817/pexels-photo-12737817.jpeg?auto=compress&cs=tinysrgb&w=600"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+        <RestCard
+          restName="Niyaaz Biryani"
+          restCuisine="Biryani"
+          restImgUrl="https://images.pexels.com/photos/12737817/pexels-photo-12737817.jpeg?auto=compress&cs=tinysrgb&w=600"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+        <RestCard
+          restName="Niyaaz Biryani"
+          restCuisine="Biryani"
+          restImgUrl="https://images.pexels.com/photos/12737817/pexels-photo-12737817.jpeg?auto=compress&cs=tinysrgb&w=600"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+        <RestCard
+          restName="Niyaaz Biryani"
+          restCuisine="Biryani"
+          restImgUrl="https://images.pexels.com/photos/12737817/pexels-photo-12737817.jpeg?auto=compress&cs=tinysrgb&w=600"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+        <RestCard
+          restName="Niyaaz Biryani"
+          restCuisine="Biryani"
+          restImgUrl="https://images.pexels.com/photos/12737817/pexels-photo-12737817.jpeg?auto=compress&cs=tinysrgb&w=600"
+          restRating="4.2"
+          restCuisinePrice="500"
+        ></RestCard>
+      </div>
+    </main>
+  );
+};
+
 const App = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header></Header>
+      <Main></Main>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.querySelector(".root"));
+
 root.render(<App />);
