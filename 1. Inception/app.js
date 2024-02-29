bbd638665cc32c3ac955d5dec49cd5ce5f535f0a
jeevan <jeevanhrx@gmail.com>
2024-02-29 13:41:35 +0530

    igniting our app

--- 1. Inception/app.js
+++
@@ -1,8 +0,0 @@
-const h1 = React.createElement(
-  "h1",
-  { id: "main-heading", className: "heading" },
-  "Hello React"
-);
-console.log(h1);
-const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(h1);
